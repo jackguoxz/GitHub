a339f2f3ea9225d29247085e8f80ebbb728fc103
jackguoxz <jackguoxz@hotmail.com>
2016-11-25 11:11:41 +0800

    Update ANodeTask.java

--- ANodeTask.java
+++ ANodeTask.java
@@ -3,7 +3,6 @@ package com.dianping.workflow;
 /**
  * 模拟在每个node上面的任务
  * <p>
- * Created by yuchao on 16/11/24.
  */
 public abstract class ANodeTask {
     public abstract void doTask(ANode node);
